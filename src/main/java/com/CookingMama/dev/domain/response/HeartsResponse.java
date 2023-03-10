f4ef0b167e40835701bf83cee9e7330723577343
pshoon25 <pshoon25@naver.com>
2023-03-10 12:17:46 +0900

    heartsId add

--- src/main/java/com/CookingMama/dev/domain/response/HeartsResponse.java
+++ src/main/java/com/CookingMama/dev/domain/response/HeartsResponse.java
@@ -10,12 +10,14 @@ import java.util.List;
 @Getter@Setter@ToString
 @AllArgsConstructor@NoArgsConstructor
 public class HeartsResponse {
+    private Long heartsId;
     private String itemOption;
     private Integer count;
     private String itemName;
     private Integer itemPrice;
 
     public HeartsResponse(Hearts hearts){
+        this.heartsId = hearts.getId();
         this.itemOption = hearts.getOption();
         this.count = hearts.getCount();
         this.itemPrice = hearts.getItem().getItemPrice();
